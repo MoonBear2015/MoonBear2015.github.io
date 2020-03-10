40f6fa400d9f71891a6ad1c80c6e0fcdade34c73
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2020-03-11 08:31:23 +0900

    R1.38

--- sc_news.js
+++ sc_news.js
@@ -7,7 +7,7 @@ function set_news() {
     html += '<h1>';
     html += 'NEWS';
     html += '<small>';
-    html += 'R01.37';
+    html += 'R01.38';
     html += '</small>';
     html += '</h1>';
     html += '</div>';
