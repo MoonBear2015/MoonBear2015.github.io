cd93df299042964ccf77a8e8c3d979618245206c
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2020-04-02 05:38:13 +0900

    P1.25

--- sc_poem.js
+++ sc_poem.js
@@ -9,7 +9,7 @@ function set_poem() {
     html += '<h1>';
     html += 'POEM';
     html += '<small>';
-    html += 'P01.24';
+    html += 'P01.25';
     html += '</small>';
     html += '</h1>';
     html += '</div>';
