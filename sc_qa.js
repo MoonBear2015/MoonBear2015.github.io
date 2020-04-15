98f336c41a49868f17b72d9df8ac858d2e734042
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2020-04-16 04:42:18 +0900

    Q0.03

--- sc_qa.js
+++ sc_qa.js
@@ -29,48 +29,9 @@ function make_qa() {
     html += 'padding:    10px; ';
     html += 'border:     0.5px solid #606060;';
     html += 'border-radius:  1%;';
-    // html += 'background: ';
-    // html += 'linear-gradient(0deg,rgba(0,0,0,0.6),rgba(0,0,0,0.8)),';
-    // html += 'url(./pics/@PIC_DO@);';
-    // html += 'background-position: center center;';
-    // html += 'background-size: cover;';
     html += '">';
     html += Make_Q();
     html += Make_A();
-    // html += '<h2 id="qa_title">';
-    // html += '<span style="border-bottom: solid 2px #FFFFFF;">';
-    // html += '@QA_TITLE@';
-    // html += '</span>';
-    // html += '</h2>';
-    // html += '<div id="news_pic_L">';
-    // html += '<figure>';
-    // html += '<img src="pics/@PIC_WHAT@" width="300px">';
-    // html += '</figure>';
-    // html += '</div>';
-    // html += '<p id="news_doc">';
-    // html += '　@NEWS_DOC@';
-    // for(let i = 0;i < rnd_minmax(2,3);i++)
-    // {
-    //     html += '@CONECT@、';
-    //     html += '@NEWS_DOC@';
-    // }
-    // html += '</p>';
-    // html += '<div id="news_pic_R">';
-    // html += '<figure>';
-    // html += '<img src="pics/@PIC_DO@" width="300px">';
-    // html += '</figure>';
-    // html += '</div>';
-    // html += '<p id="news_doc">';
-    // html += '　@NEWS_DOC@';
-    // for(let i = 0;i < rnd_minmax(3,5);i++)
-    // {
-    //     html += '@CONECT@、';
-    //     html += '@NEWS_DOC@';
-    // }
-    // html += '</p>';
-    // html += '<br>';
-    // html += '@WRITER@';
-    // html += '<br>';
     //---- this Q&A END
     html += '</div>';
     let maker = new qa_docs_maker();
@@ -105,7 +66,7 @@ function Make_Q() {
     html += '@QUESTER@';
     html += '<br>';
     html += '<p id="q_title">';
-    html += '私の@L_PART@が、@KEY@ばかりして@THINK@しています。';
+    html += '@Q_TITLE@';
     html += '</p>';
     html += '<p id="q_doc">';
     html += '私の@L_PART@が、@KEY@ばかりして@THINK@しています。';
@@ -151,11 +112,13 @@ function Make_A() {
 class qa_docs_maker extends news_docs_maker {
     constructor() {
         super();
+        this.dic_push(new selector_q_title());
         this.dic_push(new selector_pic_q());
         this.dic_push(new selector_pic_a());
         this.dic_push(new selector_quester());
         this.dic_push(new selector_advicer());
         this.dic_push(new locker_part());
+        this.dic_push(new locker_KEY());
     }
 }
 class selector_quester extends ItmArray {
@@ -197,7 +160,9 @@ class selector_pic_q extends SctItm_Selector {
             new SctItm('Q01.jpg'),
             new SctItm('Q02.jpg'),
             new SctItm('Q03.jpg'),
-            new SctItm('Q04.jpg')
+            new SctItm('Q04.jpg'),
+            new SctItm('Q05.jpg'),
+            new SctItm('Q06.jpg')
         ];
     }
 }
@@ -208,7 +173,21 @@ class selector_pic_a extends SctItm_Selector {
             new SctItm('A01.jpg'),
             new SctItm('A02.jpg'),
             new SctItm('A03.jpg'),
-            new SctItm('A04.jpg')
+            new SctItm('A04.jpg'),
+            new SctItm('A05.jpg'),
+            new SctItm('A06.jpg')
+        ];
+    }
+}
+class selector_q_title extends SctItm_Selector {
+    constructor() {
+        super('@Q_TITLE@');
+        this.itms = [
+            new SctItm('@L_PART@が@L_KEY@ばかりして@THINK@しています。'),
+            new SctItm('@L_PART@が@L_KEY@を起こしてしまいました。'),
+            new SctItm('助けて！ @L_PART@がまるで@NICK@なんです。'),
+            new SctItm('私の@L_PART@が@NICK@にしか見えません。'),
+            new SctItm('私の@L_PART@が@NICK@にしか見えません。')
         ];
     }
 }
--- sc_qa.ts
+++ sc_qa.ts
@@ -37,64 +37,15 @@ function make_qa()
     html += 'padding:    10px; ';
     html += 'border:     0.5px solid #606060;';
     html += 'border-radius:  1%;';
-    // html += 'background: ';
-    // html += 'linear-gradient(0deg,rgba(0,0,0,0.6),rgba(0,0,0,0.8)),';
-    // html += 'url(./pics/@PIC_DO@);';
-    // html += 'background-position: center center;';
-    // html += 'background-size: cover;';
     html += '">';
 
     html += Make_Q();
     html += Make_A();
 
-
-    // html += '<h2 id="qa_title">';
-    // html += '<span style="border-bottom: solid 2px #FFFFFF;">';
-    // html += '@QA_TITLE@';
-    // html += '</span>';
-    // html += '</h2>';
-
-    // html += '<div id="news_pic_L">';
-    // html += '<figure>';
-    // html += '<img src="pics/@PIC_WHAT@" width="300px">';
-    // html += '</figure>';
-    // html += '</div>';
-    
-    // html += '<p id="news_doc">';
-    // html += '　@NEWS_DOC@';
-    // for(let i = 0;i < rnd_minmax(2,3);i++)
-    // {
-    //     html += '@CONECT@、';
-    //     html += '@NEWS_DOC@';
-    // }
-    // html += '</p>';
-
-    // html += '<div id="news_pic_R">';
-    // html += '<figure>';
-    // html += '<img src="pics/@PIC_DO@" width="300px">';
-    // html += '</figure>';
-    // html += '</div>';
-
-    // html += '<p id="news_doc">';
-    // html += '　@NEWS_DOC@';
-    // for(let i = 0;i < rnd_minmax(3,5);i++)
-    // {
-    //     html += '@CONECT@、';
-    //     html += '@NEWS_DOC@';
-    // }
-    // html += '</p>';
-
-    // html += '<br>';
-    
-    // html += '@WRITER@';
-
-    // html += '<br>';
-
     //---- this Q&A END
     html += '</div>';
 
     let maker = new qa_docs_maker();
-
 
     let cnt = 0;
     while(true)
@@ -132,7 +83,7 @@ function Make_Q() : string {
     html += '<br>';
 
     html += '<p id="q_title">';
-    html += '私の@L_PART@が、@KEY@ばかりして@THINK@しています。';
+    html += '@Q_TITLE@';
     html += '</p>';
 
     html += '<p id="q_doc">';
@@ -192,11 +143,14 @@ function Make_A() : string {
 class qa_docs_maker extends news_docs_maker {
     constructor(){
         super();
+        this.dic_push(new selector_q_title());
+
         this.dic_push(new selector_pic_q());
         this.dic_push(new selector_pic_a());
         this.dic_push(new selector_quester());
         this.dic_push(new selector_advicer());
         this.dic_push(new locker_part());
+        this.dic_push(new locker_KEY());
     }
 }
 
@@ -261,6 +215,10 @@ class selector_pic_q extends SctItm_Selector implements ISctItm_Selector {
             new SctItm('Q03.jpg')
             ,
             new SctItm('Q04.jpg')
+            ,
+            new SctItm('Q05.jpg')
+            ,
+            new SctItm('Q06.jpg')
         ]
     }
 }
@@ -275,9 +233,31 @@ class selector_pic_a extends SctItm_Selector implements ISctItm_Selector {
             new SctItm('A03.jpg')
             ,
             new SctItm('A04.jpg')
+            ,
+            new SctItm('A05.jpg')
+            ,
+            new SctItm('A06.jpg')
         ]
     }
 }
+
+class selector_q_title extends SctItm_Selector implements ISctItm_Selector {
+    constructor(){
+        super('@Q_TITLE@');
+        this.itms = [
+            new SctItm('@L_PART@が@L_KEY@ばかりして@THINK@しています。')
+            ,
+            new SctItm('@L_PART@が@L_KEY@を起こしてしまいました。')
+            ,
+            new SctItm('助けて！ @L_PART@がまるで@NICK@なんです。')
+            ,
+            new SctItm('私の@L_PART@が@NICK@にしか見えません。')
+            ,
+            new SctItm('私の@L_PART@が@NICK@にしか見えません。')
+        ]
+    }
+}
+
     
 
 // 問題の相手
