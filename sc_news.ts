2748ce86cfb52c5052f7c4cd47c5a3db3884292c
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2020-04-18 08:43:39 +0900

    Revert "Q0.05r"
    This reverts commit 4666242e87da21ed182252fd72ac3657d275602b.

--- sc_news.ts
+++ sc_news.ts
@@ -9,7 +9,7 @@ function set_news()
     html += '<h1>';
     html += 'NEWS';
     html += '<small>';
-    html += 'N02.23';
+    html += 'N02.22';
     html += '</small>';
     html += '</h1>';
     html += '</div>';
@@ -758,10 +758,10 @@ class selector_whats extends SctItm_SelectLocker implements ISctItm_Selector {
             new SctItm('韓国','WHAT/koria.jpg')
             ,
             new SctItm('エジプト','WHAT/pyramid.jpg')
-            // ,
-            // new SctItm('ガミラス帝国','WHAT/gamilas.jpg')
-            // ,
-            // new SctItm('イスカンダル','WHAT/iskandar.jpg')
+            ,
+            new SctItm('ガミラス帝国','WHAT/gamilas.jpg')
+            ,
+            new SctItm('イスカンダル','WHAT/iskandar.jpg')
             // ,
             // new SctItm('経済','WHAT/Economy.jpg')
             ,
@@ -866,42 +866,42 @@ class selector_whats extends SctItm_SelectLocker implements ISctItm_Selector {
             new SctItm('地獄','WHAT/enma.jpg')
             ,
             new SctItm('南国','WHAT/seaside.jpg')
-            // ,
-            // new SctItm('ディズニーランド','WHAT/disney.jpg')
-            // ,
-            // new SctItm('ディズニーランド','WHAT/disney2.jpg')
-            // ,
-            // new SctItm('ひらかたパーク','WHAT/hirapa-.jpg')
-            // ,
-            // new SctItm('ひらかたパーク','WHAT/hirapa-2.jpg')
-            // ,
-            // new SctItm('ひらかたパーク','WHAT/hirapa-3.jpg')
-            // ,
-            // new SctItm('ＵＳＪ','WHAT/usj.jpg')
-            // ,
-            // new SctItm('ＵＳＪ','WHAT/usj2.jpg')
-            // ,
-            // new SctItm('としまえん','WHAT/toshimaen.jpg')
-            // ,
-            // new SctItm('上野動物園','WHAT/ueno_zoo.jpg')
-            // ,
-            // new SctItm('ジオン公国','WHAT/jion.jpg')
+            ,
+            new SctItm('ディズニーランド','WHAT/disney.jpg')
+            ,
+            new SctItm('ディズニーランド','WHAT/disney2.jpg')
+            ,
+            new SctItm('ひらかたパーク','WHAT/hirapa-.jpg')
+            ,
+            new SctItm('ひらかたパーク','WHAT/hirapa-2.jpg')
+            ,
+            new SctItm('ひらかたパーク','WHAT/hirapa-3.jpg')
+            ,
+            new SctItm('ＵＳＪ','WHAT/usj.jpg')
+            ,
+            new SctItm('ＵＳＪ','WHAT/usj2.jpg')
+            ,
+            new SctItm('としまえん','WHAT/toshimaen.jpg')
+            ,
+            new SctItm('上野動物園','WHAT/ueno_zoo.jpg')
+            ,
+            new SctItm('ジオン公国','WHAT/jion.jpg')
             ,
             new SctItm('ナチス・ドイツ','WHAT/nachi.jpg')
-            // ,
-            // new SctItm('ホビット床','WHAT/Shire.jpg')
-            // ,
-            // new SctItm('銀河帝国','WHAT/GalaxyEmpire.jpg')
-            // ,
-            // new SctItm('デス・スター','WHAT/deathstar.jpg')
-            // ,
-            // new SctItm('暗黒面','WHAT/darkside.jpg')
+            ,
+            new SctItm('ホビット床','WHAT/Shire.jpg')
+            ,
+            new SctItm('銀河帝国','WHAT/GalaxyEmpire.jpg')
+            ,
+            new SctItm('デス・スター','WHAT/deathstar.jpg')
+            ,
+            new SctItm('暗黒面','WHAT/darkside.jpg')
             ,
             new SctItm('ローマ帝国','WHAT/Rome.jpg')
             ,
             new SctItm('未来','WHAT/future.jpg')
-            // ,
-            // new SctItm('近未来','WHAT/nearfuture.jpg')
+            ,
+            new SctItm('近未来','WHAT/nearfuture.jpg')
             ,
             new SctItm('縄文時代','WHAT/joumon.jpg')
             ,
