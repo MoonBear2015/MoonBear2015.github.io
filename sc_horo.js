f458e03a0c1ac69548e2b1203a821024ce31dfa2
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2020-08-05 08:56:12 +0900

    H0.04

--- sc_horo.js
+++ sc_horo.js
@@ -7,7 +7,7 @@ function set_horo() {
     html += '<h1>';
     html += 'Horoscope';
     html += '<small>';
-    html += ' H00.03';
+    html += ' H00.04';
     html += '</small>';
     html += '</h1>';
     html += '</div>';
@@ -27,48 +27,70 @@ function make_horo() {
     let dt = new Date();
     let html = '';
     let days = SplitYearDays(cntHoro);
-    for (let i = 0; i < 2; i++) {
-        html += '<div id="horo_box">';
-        for (let j = 0; j < cntHoro; j++) {
-            html += '<div id="horo_line">';
-            html += '<div id="horo_colicon">';
-            html += '<img src="pics/@ICON_HORO@" height="60px">';
-            html += '</div>';
-            html += '<div id="horo_colname">';
-            html += '@F_ANIMAL@';
-            html += '</div>';
-            html += '<div id="horo_colstart">';
-            html += date_MD_string(days[j].st);
-            html += '</div>';
-            html += '<div id="horo_colto">';
-            html += ' ~ ';
-            html += '</div>';
-            html += '<div id="horo_colend">';
-            html += date_MD_string(days[j].ed);
-            html += '</div>';
-            html += '</div>';
-        }
+    html += '<div id="horo_box">';
+    for (let j = 0; j < cntHoro; j++) {
+        html += '<div id="horo_line">';
+        html += '<div id="horo_colicon">';
+        html += '<img src="pics/@ICON_HORO@" height="60px">';
         html += '</div>';
-        let cnt = 0;
-        while (true) {
-            html = maker.gene_docs(html);
-            cnt++;
-            let chk = html.indexOf('@');
-            if (chk < 0)
-                break;
-            if (cnt > 10) {
-                alert('over work : ' + chk.toString());
-                break;
-            }
+        html += '<div id="horo_colname">';
+        html += '@F_ANIMAL_B@';
+        html += '</div>';
+        html += '<div id="horo_colstart">';
+        html += date_MD_string(days[j].st);
+        html += '</div>';
+        html += '<div id="horo_colto">';
+        html += ' ~ ';
+        html += '</div>';
+        html += '<div id="horo_colend">';
+        html += date_MD_string(days[j].ed);
+        html += '</div>';
+        html += '</div>';
+    }
+    html += '</div>';
+    let cnt0 = 0;
+    while (true) {
+        html = maker.gene_docs(html);
+        cnt0++;
+        let chk = html.indexOf('@');
+        if (chk < 0)
+            break;
+        if (cnt0 > 10) {
+            alert('over work : ' + chk.toString());
+            break;
         }
-        fixAnimal.Reset();
+    }
+    fixAnimal.Reset();
+    html += '<div id="horo_sent_box">';
+    for (let j = 0; j < cntHoro; j++) {
+        html += '<div id="horo_sent">';
+        html += '<div id="horo_sent_icon">';
+        html += '<img src="pics/@ICON_HORO@" width="100%">';
+        html += '</div>';
+        html += '<div id="horo_sent_icon_name" width="100%">';
+        html += '@F_ANIMAL@';
+        html += '</div>';
+        html += '</div>';
+    }
+    html += '</div>';
+    let cnt1 = 0;
+    while (true) {
+        html = maker.gene_docs(html);
+        cnt1++;
+        let chk = html.indexOf('@');
+        if (chk < 0)
+            break;
+        if (cnt1 > 10) {
+            alert('over work : ' + chk.toString());
+            break;
+        }
     }
     return html;
 }
 // （固定）ステータス
 class Fix_animal extends SctItm_FixSeq {
     constructor() {
-        super('@F_ANIMAL@', '', '@ICON_HORO@');
+        super('@F_ANIMAL@', '@F_ANIMAL_B@', '@ICON_HORO@');
         this.Add(itms_horo_animal);
     }
 }
