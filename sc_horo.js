c426b2b71d2dee3388dcad246a6ebad469a2d24f
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2020-08-13 10:11:39 +0900

    H0.20

--- sc_horo.js
+++ sc_horo.js
@@ -7,7 +7,7 @@ function set_horo() {
     html += '<h1>';
     html += 'Horoscope';
     html += '<small>';
-    html += ' H00.19';
+    html += ' H00.20';
     html += '</small>';
     html += '</h1>';
     html += '</div>';
