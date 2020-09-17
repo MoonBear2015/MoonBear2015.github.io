c76a72d2fab0b9e1dcb7caa7ea22491af5908d48
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2020-09-18 08:52:14 +0900

    n0.04

--- sc/sc_news.ts
+++ sc/sc_news.ts
@@ -9,7 +9,7 @@ function set_news()
     html += "<h2>";
     html += "NEWS";
     html += "<small>";
-    html += " N0.02";
+    html += " N0.04";
     html += "</small>";
     html += "</h2>";
     html += "</div>";
@@ -17,9 +17,21 @@ function set_news()
 
     for(let i = 0; i < 20; i++){
         html += "<p>[" + i.toString() + "]</p>";
+
+        html += "<div id='box'>";
         html += "<br>";
         html += "<h3>日本沈没</h3>";
-
+        html += "<br>";
+        html += "<figure id='fig_left'>";
+        // html += "<img src='./img/place/Japan.jpg' width='300px' alt='NEWS'>";
+        html += "<img src='./img/place/Japan.jpg' alt='NEWS'>";
+        html += "</figure>";
+        html += "<p>　";
+        for(let j = 0;j  < 100; j++) {
+            html += "日本が沈没しました。";
+        }
+        html += "</p>";
+        html += "</div>";
     }
 
     set_html("index_main",html);
