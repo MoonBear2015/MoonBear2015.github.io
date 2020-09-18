c45cede92fca24f00a806b9131ac8e511329c00e
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2020-09-19 07:59:53 +0900

    n0.05

--- sc/sc_news.ts
+++ sc/sc_news.ts
@@ -1,4 +1,4 @@
-function set_news()
+const set_news = () =>
 {
     set_index_header_menu(0);
 
@@ -9,7 +9,7 @@ function set_news()
     html += "<h2>";
     html += "NEWS";
     html += "<small>";
-    html += " N0.04";
+    html += " N0.05";
     html += "</small>";
     html += "</h2>";
     html += "</div>";
@@ -20,22 +20,42 @@ function set_news()
 
         html += "<div id='box'>";
         html += "<br>";
-        html += "<h3>日本沈没</h3>";
+        html += "<h3>@country@沈没</h3>";
         html += "<br>";
         html += "<figure id='fig_left'>";
-        // html += "<img src='./img/place/Japan.jpg' width='300px' alt='NEWS'>";
         html += "<img src='./img/place/Japan.jpg' alt='NEWS'>";
         html += "</figure>";
         html += "<p>　";
         for(let j = 0;j  < 100; j++) {
-            html += "日本が沈没しました。";
+            html += "@country@が沈没しました。";
         }
         html += "</p>";
         html += "</div>";
     }
-
+    html = replace_string(html,"@country@",dic_place);
     set_html("index_main",html);
 
 }
 
+const replace_string = (inStr : string,inKey : string,inDic : string[])
+ : string => 
+{
+    let result : string = inStr;
+    let cnt : number = 0;
+    while(true) {
+        if (result.indexOf(inKey) == -1) break;
+        let after = inDic[RanMax(inDic.length)];
+        result = result.replace(inKey,after);
+        cnt++;
+        if (cnt > 1000) break;
+    }
+    alert(result);
+    return result;
+}
+
+const dic_place = [
+    "日本"
+    ,
+    "アメリカ"
+]
 
