c5bd01f07dc0bfaaa80667f120244e7acb157a0c
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2020-10-04 08:48:52 +0900

    N0.25

---
+++ sc/sc_dic.js
@@ -0,0 +1,38 @@
+"use strict";
+const dic_news_title = [
+    new WrdSt("@WHAT@@DO@", "NEWSTITLE"),
+    new WrdSt("@WHAT@の@DO@", "NEWSTITLE"),
+    new WrdSt("@DO@する@WHAT@", "NEWSTITLE"),
+    new WrdSt("@DO@した@WHAT@", "NEWSTITLE")
+];
+const dic_country = [
+    new WrdSt("日本", "WHAT,COUNTRY,ASIA", "./img/country/Japan.jpg"),
+    new WrdSt("中国", "WHAT,COUNTRY,ASIA", "./img/country/China.jpg"),
+    new WrdSt("韓国", "WHAT,COUNTRY,ASIA", "./img/country/Koria.jpg"),
+    new WrdSt("アメリカ", "WHAT,COUNTRY", "./img/country/America.jpg"),
+    new WrdSt("イギリス", "WHAT,COUNTRY,EUROPE", "./img/country/England.jpg"),
+    new WrdSt("フランス", "WHAT,COUNTRY,EUROPE", "./img/country/France.jpg"),
+    new WrdSt("イタリア", "WHAT,COUNTRY,EUROPE", "./img/country/Italy.jpg"),
+    new WrdSt("ドイツ", "WHAT,COUNTRY,EUROPE", "./img/country/German.jpg"),
+    new WrdSt("ロシア", "WHAT,COUNTRY", "./img/country/Russia.jpg"),
+    new WrdSt("インド", "WHAT,COUNTRY", "./img/country/India.jpg")
+];
+const dic_city = [
+    new WrdSt("江戸", "WHAT,CITY,ASIA", "./img/city/Edo.jpg"),
+    new WrdSt("東京", "WHAT,CITY,ASIA", "./img/city/Tokyo.jpg"),
+    new WrdSt("大阪", "WHAT,CITY,ASIA", "./img/city/Osaka.jpg"),
+    new WrdSt("香港", "WHAT,CITY,ASIA", "./img/city/Hongkong.jpg"),
+    new WrdSt("ニューヨーク", "WHAT,CITY", "./img/city/Newyork.jpg"),
+    new WrdSt("ラスベガス", "WHAT,CITY", "./img/city/LasVegas.jpg"),
+    new WrdSt("ロンドン", "WHAT,CITY,EUROPE", "./img/city/London.jpg"),
+    new WrdSt("パリ", "WHAT,CITY,EUROPE", "./img/city/Paris.jpg"),
+    new WrdSt("ローマ", "WHAT,CITY,EUROPE", "./img/city/Rome.jpg")
+];
+const dic_do = [
+    new WrdSt("破滅", "DO", "./img/ruin/BankRuin.jpg"),
+    new WrdSt("壊滅", "DO", "./img/ruin/BrokenRuin.jpg"),
+    new WrdSt("自滅", "DO", "./img/ruin/SelfRuin.jpg"),
+    new WrdSt("絶滅", "DO", "./img/ruin/CutRuin.jpg"),
+    new WrdSt("死滅", "DO", "./img/ruin/DeathRuin.jpg"),
+    new WrdSt("全滅", "DO", "./img/ruin/AllRuin.jpg")
+];
--- sc/sc_news.js
+++ sc/sc_news.js
@@ -7,63 +7,40 @@ const set_news = () => {
     html += "<h2>";
     html += "NEWS";
     html += "<small>";
-    html += " N0.24";
+    html += " N0.25";
     html += "</small>";
     html += "</h2>";
     html += "</div>";
     html += "<br>";
-    html += "<br>";
-    html += "---------------<br>";
     let base = new DictionaryBase();
+    base.AddWrds(dic_news_title);
     base.AddWrds(dic_country);
+    base.AddWrds(dic_city);
     base.AddWrds(dic_do);
-    html += cr_br(base.toString());
-    html += "<br>";
-    html += "---------------<br>";
-    for (let key in base.selectors) {
-        html += "****** " + key + "<br>";
-        for (let i = 0; i < 10; i++) {
-            let wrd = base.selectors[key].next();
-            if (wrd) {
-                html += "***> " + wrd.toString() + "<br>";
-            }
-        }
-    }
-    html += "<br>";
-    html += "---------------<br>";
     for (let i = 0; i < 20; i++) {
         html += "<p>[" + i.toString() + "]</p>";
         html += "<div id='box'>";
         html += "<br>";
-        html += "<h3>@COUNTRY@@DO@</h3>";
+        html += "<h3>@NEWSTITLE@</h3>";
         html += "<br>";
         html += "<figure id='fig_left'>";
-        html += "<img src='./img/place/Japan.jpg' alt='NEWS'>";
+        html += "<img src='@P_WHAT@' alt='NEWS'>";
         html += "</figure>";
         html += "<p>　";
-        for (let j = 0; j < 100; j++) {
+        for (let j = 0; j < 30; j++) {
+            html += "@COUNTRY@が@DO@しました。";
+        }
+        html += "</p>";
+        html += "<figure id='fig_right'>";
+        html += "<img src='@P_DO@' alt='NEWS'>";
+        html += "</figure>";
+        html += "<p>　";
+        for (let j = 0; j < 30; j++) {
             html += "@COUNTRY@が@DO@しました。";
         }
         html += "</p>";
         html += "</div>";
+        html = base.tagReplace(html);
     }
-    html = base.tagReplace(html);
     set_html("index_main", html);
 };
-const dic_country = [
-    new WrdSt("日本", "COUNTRY,ASIA"),
-    new WrdSt("中国", "COUNTRY,ASIA"),
-    new WrdSt("アメリカ", "COUNTRY")
-];
-const dic_do = [
-    new WrdSt("破滅", "DO"),
-    new WrdSt("壊滅", "DO"),
-    new WrdSt("自滅", "DO"),
-    new WrdSt("絶滅", "DO"),
-    new WrdSt("死滅", "DO"),
-    new WrdSt("全滅", "DO")
-];
-const dic_place = [
-    "日本",
-    "アメリカ"
-];
