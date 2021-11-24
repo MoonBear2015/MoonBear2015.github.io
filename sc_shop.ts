81fb830519f457dd7379b21da057eca4c91adb30
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2021-11-25 08:11:47 +0900

    footer 02

--- sc_shop.ts
+++ sc_shop.ts
@@ -258,7 +258,7 @@ function make_site_footer(): string {
 
     html += '<div id="foot_cm">'; // cm
     html += '<div id="foot_pic">';
-    html += '<img src="pics/@PIC_SHOP@" width="100%">';
+    html += '<img src="pics/@PIC_SHOP@" width="70%">';
     html += '</div>';
 
     html += '<div id="foot_title">';
