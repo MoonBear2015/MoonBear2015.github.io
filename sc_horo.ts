56b90ac9b9d778080a6cfee2409edf9a3779b174
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2022-01-10 21:47:47 +0900

    H00.55

--- sc_horo.ts
+++ sc_horo.ts
@@ -9,7 +9,7 @@ function set_horo()
     html += '<h1>';
     html += 'Horoscope';
     html += '<small>';
-    html += ' H00.54';
+    html += ' H00.55';
     html += '</small>';
     html += '</h1>';
     html += '</div>';
