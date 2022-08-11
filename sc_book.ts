5262402d8076def5c730000af5bc78c94deb70d2
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2022-08-11 13:49:06 +0900

    B00.06

--- sc_book.ts
+++ sc_book.ts
@@ -9,7 +9,7 @@ function set_book()
     html += '<h1>';
     html += 'Q&A';
     html += '<small>';
-    html += ' B00.05';
+    html += ' B00.06';
     html += '</small>';
     html += '</h1>';
     html += '</div>';
--- sc_news.ts
+++ sc_news.ts
@@ -366,7 +366,7 @@ class locker_bookwriter
     }
 
     get first_itm() : SctItm {
-        return new SctItm(this.created_name.html_BOOKWRITER(150),'');
+        return new SctItm(this.created_name.html_BOOKWRITER(80),'');
     }
 }
 
