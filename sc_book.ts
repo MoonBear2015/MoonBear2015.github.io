8c2c7ec110bec46c28502a17ea76a6b474a0291e
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2022-09-06 08:56:36 +0900

    B01.11

--- sc_book.ts
+++ sc_book.ts
@@ -9,7 +9,7 @@ function set_book()
     html += '<h1>';
     html += 'Book';
     html += '<small>';
-    html += ' B01.10';
+    html += ' B01.11';
     html += '</small>';
     html += '</h1>';
     html += '</div>';
