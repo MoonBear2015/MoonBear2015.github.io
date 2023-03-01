e286c522d2ca888eb96e4a42c4cd06908ed770df
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2023-03-02 06:09:41 +0900

    M2.00

--- sc_main.js
+++ sc_main.js
@@ -31,10 +31,13 @@ function make_site_header() {
     html += '<a href="https://www.free-counter.jp/"><img src="https://www.f-counter.net/j/61/1677576045/" alt="アクセスカウンター"></a>';
     html += '<h1>';
     html += '「空虚」';
-    html += '<small>';
-    html += 'M02.00 パソコンのブラウザでご覧下さい';
-    html += '</small>';
+    // html += '<small>';
+    // html += 'M02.00';
+    // html += '</small>';
     html += '</h1>';
+    html += '<small>';
+    html += 'パソコンのブラウザでご覧下さい';
+    html += '</small>';
     html += '</div>';
     html += '<div id="header_menu">';
     html += make_header_menu(0);
