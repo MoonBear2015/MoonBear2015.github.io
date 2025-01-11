8360e5d33f65a909efd830f61dd0a15889ea835a
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2025-01-12 08:54:45 +0900

    G_00.09

--- _game/test.js
+++ _game/test.js
@@ -1,2 +1,22 @@
 "use strict";
-alert("こんにちは");
+function canvas_sankaku(canvas, width, height) {
+    if (canvas == undefined)
+        return;
+    if (canvas == null)
+        return;
+    var context = canvas.getContext("2d");
+    if (context == null)
+        return;
+    context.beginPath();
+    var x1 = g_rnd(0, width);
+    var x2 = g_rnd(0, width);
+    var y1 = g_rnd(0, height);
+    var y2 = g_rnd(0, height);
+    context.moveTo(x1, y1);
+    context.lineTo(x1, y2);
+    context.lineTo(x2, y2);
+    context.lineTo(x1, y1);
+    context.closePath();
+    context.fillStyle = "#" + g_rnd(0xFFFFFF).toString(16);
+    context.fill();
+}
--- _game/test.ts
+++ _game/test.ts
@@ -1,1 +1,22 @@
-alert("こんにちは");
+function canvas_sankaku (canvas : HTMLCanvasElement,
+    width : number,
+    height : number
+) {
+    if (canvas == undefined) return;
+    if (canvas == null) return;
+    var context = canvas.getContext("2d");
+    if (context == null) return;
+    context.beginPath();
+    var x1 : number = g_rnd(0,width);
+    var x2 : number = g_rnd(0,width);
+    var y1 : number = g_rnd(0,height);
+    var y2 : number = g_rnd(0,height);
+    context.moveTo(x1,y1);
+    context.lineTo(x1,y2);
+    context.lineTo(x2,y2);
+    context.lineTo(x1,y1);
+    context.closePath();
+    context.fillStyle = "#" + g_rnd(0xFFFFFF).toString(16);
+    context.fill();
+}
+
