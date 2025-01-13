1ac57ce1232d8b73c4dbf0715fbafcc1b6368697
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2025-01-13 20:24:46 +0900

    G_00.12

--- _game/test.js
+++ _game/test.js
@@ -1,31 +1,70 @@
 "use strict";
+var BoxSts = Array(12).fill(0);
+var BOX_OFF = 0;
+var BOX_ON = 1;
+var BOX_HIT = 2;
+var Image_Off = new Image();
+var Image_On = new Image();
+var Image_Hit = new Image();
+var Image_Back = new Image();
+function PicLoad() {
+    Image_Off.src = ".\\pics\\test\\box.png";
+    Image_On.src = ".\\pics\\test\\box_blank.png";
+    Image_Hit.src = ".\\pics\\test\\box_max.png";
+    Image_Back.src = ".\\pics\\test\\umi.jpg";
+}
+function changeBox() {
+    for (let i = 0; i < BoxSts.length; i++) {
+        if (g_rnd(9) > 3) {
+            BoxSts[i] = BOX_OFF;
+        }
+        else {
+            BoxSts[i] = BOX_ON;
+        }
+    }
+}
 function canvas_writer(canvas, width, height) {
     if (canvas == undefined)
         return;
     if (canvas == null)
         return;
-    var context = canvas.getContext("2d");
+    let context = canvas.getContext("2d");
     if (context == null)
         return;
-    context.fillStyle = "black";
-    context.fillRect(0, 0, width, height);
-    let mx = width / 15;
-    let my = height / 15;
-    let lx = width - mx;
-    let ly = height - my;
-    let x1 = width / 2;
-    let y1 = height / 15;
-    let x2 = lx;
-    let y2 = ly;
-    context.beginPath();
-    context.moveTo(mx, ly);
-    context.arcTo(x1, y1, x2, y2, 40);
-    context.lineTo(x2, y2);
-    context.fillStyle = randomColor();
-    context.fill();
-    context.lineWidth = width / 30;
-    context.strokeStyle = randomColor();
-    context.stroke();
+    let BACK_SIZE = height / 4;
+    let BOX_W = width / 4;
+    let BOX_H = height / 4;
+    context.drawImage(Image_Back, 0, 0, width, BACK_SIZE);
+    for (let i = 0; i < BoxSts.length; i++) {
+        let x = (i % 4) * BOX_W;
+        let y = BACK_SIZE + Math.floor(i / 4) * BOX_H;
+        switch (BoxSts[i]) {
+            case BOX_OFF:
+                context.drawImage(Image_Off, x, y, BOX_W, BOX_H);
+                break;
+            case BOX_ON:
+                context.drawImage(Image_On, x, y, BOX_W, BOX_H);
+                break;
+            case BOX_HIT:
+                context.drawImage(Image_Hit, x, y, BOX_W, BOX_H);
+                break;
+        }
+    }
+}
+function touchHandler(x, y, canvas, width, height) {
+    let px = x;
+    let py = y;
+    let BACK_SIZE = height / 4;
+    let BOX_W = width / 4;
+    let BOX_H = height / 4;
+    let px2 = Math.floor(px / BOX_W);
+    let py2 = Math.floor((py - BACK_SIZE) / BOX_H);
+    let no = py2 * 4 + px2;
+    console.log(px2 + "," + py2 + "=" + no);
+    if (BoxSts[no] == BOX_ON) {
+        BoxSts[no] = BOX_HIT;
+    }
+    canvas_writer(canvas, width, height);
 }
 let toRad = (deg) => deg * Math.PI / 180;
 let randomColor = () => "#" + g_rnd(0xFFFFFF).toString(16);
--- _game/test.ts
+++ _game/test.ts
@@ -1,37 +1,91 @@
+
+var BoxSts : number[] = Array(12).fill(0);
+var BOX_OFF : number = 0;
+var BOX_ON : number = 1;
+var BOX_HIT : number = 2;
+
+var Image_Off : HTMLImageElement = new Image();
+var Image_On : HTMLImageElement = new Image();
+var Image_Hit : HTMLImageElement = new Image();
+var Image_Back : HTMLImageElement = new Image();
+
+function PicLoad() {
+    Image_Off.src = ".\\pics\\test\\box.png";
+    Image_On.src = ".\\pics\\test\\box_blank.png";
+    Image_Hit.src = ".\\pics\\test\\box_max.png";
+    Image_Back.src = ".\\pics\\test\\umi.jpg";
+}
+
+function changeBox() {
+    for(let i = 0;i < BoxSts.length; i++) {
+        if (g_rnd(9) > 3) {
+            BoxSts[i] = BOX_OFF;
+        } else {
+            BoxSts[i] = BOX_ON;
+        }
+        
+    }
+}
+
+
 function canvas_writer (canvas : HTMLCanvasElement,
     width : number,
     height : number
 ) {
     if (canvas == undefined) return;
     if (canvas == null) return;
-    var context = canvas.getContext("2d");
+    let context = canvas.getContext("2d");
     if (context == null) return;
-    context.fillStyle = "black";
-    context.fillRect(0,0,width,height);
 
-    let mx : number = width / 15;
-    let my : number = height / 15;
-    let lx : number = width - mx;
-    let ly : number = height - my;
+    let BACK_SIZE : number = height / 4;
+    let BOX_W : number = width / 4;
+    let BOX_H : number = height / 4;
 
-    let x1 : number = width / 2;
-    let y1 : number = height / 15;
-    let x2 : number = lx;
-    let y2 : number = ly;
 
-    context.beginPath();
-    context.moveTo(mx,ly);
-    context.arcTo(x1,y1,x2,y2,40);
-    context.lineTo(x2,y2);
+    context.drawImage(Image_Back,0,0,width,BACK_SIZE);
 
-    context.fillStyle = randomColor();
-    context.fill();
-    context.lineWidth = width / 30;
-    context.strokeStyle = randomColor();
-    context.stroke();
+    for(let i : number = 0; i < BoxSts.length; i++)
+    {
+        let x = ( i % 4) * BOX_W;
+        let y = BACK_SIZE + Math.floor(i / 4) * BOX_H;
+        switch(BoxSts[i]) {
+            case BOX_OFF:
+                context.drawImage(Image_Off,x,y,BOX_W,BOX_H);
+                break;
+        case BOX_ON:
+                context.drawImage(Image_On,x,y,BOX_W,BOX_H);
+                break;
+            case BOX_HIT:
+                context.drawImage(Image_Hit,x,y,BOX_W,BOX_H);
+                break;
+        }
+    }
+}
 
+function touchHandler(
+    x : number,
+    y : number,
+    canvas : HTMLCanvasElement,
+    width : number,
+    height : number
+) {
+    let px = x;
+    let py = y;
+    let BACK_SIZE : number = height / 4;
+    let BOX_W : number = width / 4;
+    let BOX_H : number = height / 4;
+
+    let px2 = Math.floor(px / BOX_W);
+    let py2 = Math.floor((py - BACK_SIZE) / BOX_H);
+    let no = py2 * 4 + px2;
+    console.log(px2 + "," + py2 + "=" + no);
+    if (BoxSts[no] == BOX_ON) {
+        BoxSts[no] = BOX_HIT;
+    }
+    canvas_writer(canvas,width,height);
 }
 
 let toRad = (deg : number) : number => deg * Math.PI / 180;
 
 let randomColor = () :string => "#" + g_rnd(0xFFFFFF).toString(16);
+
