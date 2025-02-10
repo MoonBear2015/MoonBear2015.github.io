6853ca1a8d929a72c7a39e1d7ac6ca7ce44d5f14
MoonBear2015 <40275300+MoonBear2015@users.noreply.github.com>
2025-02-10 21:24:38 +0900

    C_000.006

--- _cellgame/cellgameLib.js
+++ _cellgame/cellgameLib.js
@@ -24,12 +24,28 @@ var cellgame;
         }
     }
     cellgame.Cell = Cell;
-    /** Cell 規定値 */
-    cellgame.Cells = [
+    /** Cell 規定値 00 */
+    cellgame.Cells00 = [
         new Cell(0, "　", "無し", cellgame.Colors.White, cellgame.Colors.Black),
         new Cell(1, "士", "武士", cellgame.Colors.White, cellgame.Colors.Red),
         new Cell(2, "農", "農民", cellgame.Colors.White, cellgame.Colors.DarkOrange),
         new Cell(3, "工", "職人", cellgame.Colors.White, cellgame.Colors.DarkBlue),
         new Cell(4, "商", "商人", cellgame.Colors.White, cellgame.Colors.DarkGreen)
+    ];
+    /** Cell 規定値 01 */
+    cellgame.Cells01 = [
+        new Cell(0, "　", "無し", cellgame.Colors.White, cellgame.Colors.Black),
+        new Cell(1, "侍", "武士", cellgame.Colors.White, cellgame.Colors.Red),
+        new Cell(2, "農", "農民", cellgame.Colors.White, cellgame.Colors.DarkOrange),
+        new Cell(3, "工", "職人", cellgame.Colors.White, cellgame.Colors.DarkBlue),
+        new Cell(4, "商", "商人", cellgame.Colors.White, cellgame.Colors.DarkGreen)
+    ];
+    /** Cell 規定値 02 */
+    cellgame.Cells02 = [
+        new Cell(0, "　", "無し", cellgame.Colors.White, cellgame.Colors.Black),
+        new Cell(1, "Ｓ", "Samurai：侍", cellgame.Colors.White, cellgame.Colors.Red),
+        new Cell(2, "Ｆ", "Farmer：農民", cellgame.Colors.White, cellgame.Colors.DarkOrange),
+        new Cell(3, "Ａ", "Artisans：職人", cellgame.Colors.White, cellgame.Colors.DarkBlue),
+        new Cell(4, "Ｍ", "Merchants：商人", cellgame.Colors.White, cellgame.Colors.DarkGreen)
     ];
 })(cellgame || (cellgame = {}));
--- _cellgame/cellgameLib.ts
+++ _cellgame/cellgameLib.ts
@@ -60,8 +60,8 @@ namespace cellgame {
 
     }
 
-    /** Cell 規定値 */
-    export const Cells = [
+    /** Cell 規定値 00 */
+    export const Cells00 = [
         new Cell(0,"　","無し",Colors.White,Colors.Black)
         ,
         new Cell(1,"士","武士",Colors.White,Colors.Red)
@@ -73,6 +73,31 @@ namespace cellgame {
         new Cell(4,"商","商人",Colors.White,Colors.DarkGreen)
     ];
 
+    /** Cell 規定値 01 */
+    export const Cells01 = [
+        new Cell(0,"　","無し",Colors.White,Colors.Black)
+        ,
+        new Cell(1,"侍","武士",Colors.White,Colors.Red)
+        ,
+        new Cell(2,"農","農民",Colors.White,Colors.DarkOrange)
+        ,
+        new Cell(3,"工","職人",Colors.White,Colors.DarkBlue)
+        ,
+        new Cell(4,"商","商人",Colors.White,Colors.DarkGreen)
+    ];
 
+    /** Cell 規定値 02 */
+    export const Cells02 = [
+        new Cell(0,"　","無し",Colors.White,Colors.Black)
+        ,
+        new Cell(1,"Ｓ","Samurai：侍",Colors.White,Colors.Red)
+        ,
+        new Cell(2,"Ｆ","Farmer：農民",Colors.White,Colors.DarkOrange)
+        ,
+        new Cell(3,"Ａ","Artisans：職人",Colors.White,Colors.DarkBlue)
+        ,
+        new Cell(4,"Ｍ","Merchants：商人",Colors.White,Colors.DarkGreen)
+    ];
+    
 
 }
